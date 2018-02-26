a74dff48a3ef04023e7e5200a15b317769791af7
Matshwenyego <Ashleyngoetjana@gmail.com>
2018-02-26 14:36:23 +0200

    first commit

---
+++ readfromfile/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+//Include Input/Output library 
+using System.IO;
+
+namespace readfromfile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader reader = new StreamReader("info.txt");
+
+            string line;
+
+            while (!reader.EndOfStream) {
+                line = reader.ReadLine();
+                Console.WriteLine(line);
+                
+            }
+            Console.ReadLine();
+        }
+    }
+}
